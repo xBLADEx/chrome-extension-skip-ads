db9ad46ab7713e66d3cf49747140e2d2a4d8f2ec
Rich Edmunds <rich@richedmunds.com>
2020-10-13 09:40:53 -0600

    Add trigger click to continue, refactor

--- skip-ads/content.js
+++ skip-ads/content.js
@@ -1,4 +1,21 @@
 'use strict';
 
-var video = document.querySelector('video');
-video.currentTime = video.duration;
+function skipAd() {
+  var video = document.querySelector('video');
+  video.currentTime = video.duration;
+}
+
+function continueButton() {
+  var continueButton = document.querySelector('.ytp-ad-skip-button');
+
+  if (continueButton) {
+    continueButton.click();
+  }
+}
+
+function run() {
+  skipAd();
+  continueButton();
+}
+
+run();
